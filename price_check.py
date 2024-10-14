1b690193429e040065c9846fbaf19b66708340bf
kuzhin ildar <kuzhinup@gmail.com>
2024-10-14 20:20:19 +0500

    Переименование

---
+++ price_check.py
@@ -0,0 +1,14 @@
+from web3 import Web3
+from uniswap import Uniswap
+
+
+# Адреса контрактов и токенов
+UNISWAP_V3_ROUTER_ADDRESS = "0xe592427a0aece92de3edee1f18e0157c05861564"  # Uniswap V3 router on Arbitrum
+
+# TODO: Разобраться
+# 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 с coingeko адресс usdc. Не понял
+USDC_ADDRESS = "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8"  # USDC on Arbitrum
+WETH_ADDRESS = "0x82af49447d8a07e3bd95bd0d56f35241523fbab1"  # WETH on Arbitrum
+ARB_ADDRESS = "0x912ce59144191c1204e64559fe8253a0e49e6548"   # Arbitrum token on Arbitrum
+
+# 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
