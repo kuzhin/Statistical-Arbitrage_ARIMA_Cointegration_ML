5c1bfabc9f0e4800b3e5071234d83310ae97a363
ildar <kuzhinup@gmail.com>
2025-05-03 17:22:42 +0500

    v0.2.5 Удалил файлы, которые не входят в инструкции гайда

--- stat_arb.py
+++
@@ -1,9 +0,0 @@
-from analysis.cointegration import run_cointegration_analysis
-
-if __name__ == "__main__":
-    # Запуск анализа с параметрами:
-    # - timeframe: '5m' (5 минут)
-    # - since_days: 30 (последние 30 дней)
-    # TODO: force_update = False Сделать флаг нормально
-    results = run_cointegration_analysis(timeframe='5m', since_days=10)
-    print(results)
