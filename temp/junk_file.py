8b41e0f14ba214c9eaeefadbda2bfb2ba77b5d77
Ильдар Кужин <kuzhinup@gmail.com>
2026-02-24 12:52:43 +0500

    Пересмотр проекта

--- temp/junk_file.py
+++ temp/junk_file.py
@@ -11,3 +11,16 @@ print(f"Всего токенов: {num_tokens}")
 # И выводим количество записей для каждого токена
 for token, values in data.items():
     print(f"Токен {token} содержит {len(values)} записей")
+
+
+
+# TODO: понять, как настроить вывод графика.
+#  раньше было в strategy.main_strategy.py
+# # STEP 4 - Plot trends and save for backtesting
+# print("Plotting trends...")
+# symbol_1 = '1000000PEIPEIUSDT'#"MATICUSDT"
+# symbol_2 = '1000000MOGUSDT'#"STXUSDT"
+# with open("1_price_list.json") as json_file:
+#     price_data = json.load(json_file)
+#     if len(price_data) > 0:
+#         plot_trends(symbol_1, symbol_2, price_data)
